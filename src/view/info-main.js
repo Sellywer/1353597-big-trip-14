a755b969894f918f2342c32b040bf3a6e95e1e59
Адель <selly.lady.zakirova@gmail.com>
2021-05-26 09:41:08 +0500

    Добавит верхнюю информацию по путешествию

--- src/view/info-main.js
+++ src/view/info-main.js
@@ -1,12 +1,12 @@
-import {createTotalPrice} from '../utils/event.js';
+import {createTotalPrice, getRouteEventsTitle, getAllTripDates} from '../utils/event.js';
 import AbstractView from './abstract.js';
 
 const createInfoMainTemplate = (events) => {
   return `<section class="trip-main__trip-info  trip-info">
     <div class="trip-info__main">
-      <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+      <h1 class="trip-info__title">${getRouteEventsTitle(events)}</h1>
 
-      <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+      <p class="trip-info__dates">${getAllTripDates(events)}</p>
     </div>
 
     <p class="trip-info__cost">
