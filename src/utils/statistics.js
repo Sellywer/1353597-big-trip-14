c2053ae26cceb1a7c8fd80d89aa76d6bb52e99cd
Адель <selly.lady.zakirova@gmail.com>
2021-05-28 11:25:11 +0500

    Добавит сортировку статистикеб экранирование поля цены

--- src/utils/statistics.js
+++ src/utils/statistics.js
@@ -1,21 +1,26 @@
 import dayjs from 'dayjs';
 
-
 export const getItemsUniq = (items) => [...new Set(items)];
 
 export const getCostsByTripType = (points, type) => {
-  const pointsByType = points.filter((point) => point.type === type);
-  return pointsByType.reduce((sum, item) => sum + item.basePrice, 0);
+  const pointsByType = points.points.filter((point) => point.type === type);
+  const price = pointsByType.reduce((sum, item) => sum + item.basePrice, 0);
+
+  return {type, price};
 };
 
 export const countPointsByTripType = (points, type) => {
-  return points.filter((point) => point.type === type).length;
+  const pointsCount = points.points.filter((point) => point.type === type).length;
+
+  return {type, pointsCount};
 };
 
 export const getDurationByType = (points, type) => {
-  const pointsTypes = points.filter((point) => point.type === type);
-  const duration = pointsTypes.reduce((totalDuration, point) => {
-    return totalDuration + dayjs(point.dateTo).diff(point.dateFrom,'minute');
-  }, 0);
-  return duration;
+  const pointsTypes = points.points.filter((point) => point.type === type);
+
+  let duration = 0;
+  pointsTypes.forEach((point) => {
+    duration += dayjs(point.dateTo).diff(point.dateFrom,'m');
+  });
+  return {type, duration};
 };
